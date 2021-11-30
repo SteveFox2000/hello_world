4c870e68a4dd0ea7babf71d720b2509cef805fda
SteveFox2000 <steve.GB.fox@gmail.com>
2021-11-30 14:27:45 +0200

    ldfikghj

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm1'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: '../img/svg/magento.json'
+  })
+;
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm2'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: '../img/svg/mvp.json'
+  })
+;
